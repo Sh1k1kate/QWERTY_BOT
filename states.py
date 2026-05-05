ec78eb5012e108b65ee840b201fdf0d14f3c2b1c
Sh1k1kate <100587221+Sh1k1kate@users.noreply.github.com>
2026-05-06 01:14:34 +0700

    Update states.py

--- states.py
+++ states.py
@@ -1,7 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class TechFill(StatesGroup):
-    choosing_computer = State()
     monitor = State()
     mouse = State()
     keyboard = State()
@@ -9,11 +8,5 @@ class TechFill(StatesGroup):
     sound = State()
     notes = State()
 
-class InventoryEdit(StatesGroup):
-    waiting_for_quantity = State()
-
 class InventorySearch(StatesGroup):
     waiting_for_query = State()
-
-class BarcodeScan(StatesGroup):
-    waiting_for_barcode = State()
